6c7deefdd38aae24402b2c6ca3433d1ba6e2886d
Laura Porter <laura@dxw.com>
2020-12-09 17:16:10 +0000

    Data migration to create Providers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_12_09_165202) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "providers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "organisation_name"
+    t.string "registration_number"
+    t.string "designation"
+    t.string "corporate_form"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organisation_name"], name: "index_providers_on_organisation_name"
+  end
 
 end

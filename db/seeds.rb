229bf49016bf59f885d95e439d1fc6c5f79f7e32
Laura Porter <laura@dxw.com>
2020-12-10 13:45:57 +0000

    Seed the database with the list of providers from Gov.uk
    Data taken from https://www.gov.uk/government/publications/current-registered-providers-of-social-housing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  load File.join(Rails.root, "db", "seeds", "providers.rb")
+end
